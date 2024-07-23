9665c6e76ed52e61f8a22544baf28b4bbadee672
MrSabeeh394 <msabeehsafdar394jb@gmail.com>
2024-07-23 09:40:07 +0500

    ifElse file created

---
+++ ifElse.js
@@ -0,0 +1,105 @@
+// If Else Lecture
+
+/* let marks = 9;
+if (marks > 33) {
+    console.log("Congrats! you are passed");
+}
+else {
+    console.log("You are failed");
+}
+
+let a = new Date();
+a= a.getHours()
+console.log(a);
+if (a < 12) {
+    console.log("Good Morning");
+}
+else {
+    console.log("Good Evening")
+}
+*/
+
+
+
+
+// Nested if else
+
+
+// let a = 5;
+// let b = 2;
+// let c = 1;
+// if (a > b) {
+//     if (a > c) {
+//         console.log("a is greater than c")
+//     }
+//     else {
+//         console.log("c is greater than a");
+//     }
+// }
+// else {
+//     console.log("b is greater than a")
+// }
+
+
+
+
+// let time = new Date().getHours();
+// // console.log(time);
+// if (time < 10) {
+//     console.log("Good Morning");
+// }
+// else if (time < 20) {
+//     console.log("Good Day");
+// }
+// else {
+//     console.log("Good Evening");
+// }
+
+
+
+
+
+
+// Switch statement
+let day;
+
+switch (new Date().getDay()) {
+    case 0:
+        day= "Sunday"
+        
+        break;
+    case 1:
+        day= "Monday"
+        
+        break;
+    case 2:
+        day= "Tuesday"
+        
+        break;
+    case 3:
+        day= "Wednessday"
+        
+        break;
+    case 4:
+        day= "Thursday"
+        
+        break;
+    case 5:
+        day = "Friday"
+        
+        break;
+
+    default:
+        day= "Saturday"
+        break;
+}
+console.log(day);
+
+
+
+
+
+
+
+
+
