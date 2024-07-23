61f589c242fbf763e75b7697739b755c7d0fe540
MrSabeeh394 <msabeehsafdar394jb@gmail.com>
2024-07-23 23:47:00 +0500

    Switch Statement

--- ifElse.js
+++ ifElse.js
@@ -20,6 +20,8 @@ else {
 */
 
 
+/// Switch Statment used: 
+
 
 
 // Nested if else
@@ -99,7 +101,79 @@ console.log(day);
 
 
 
+// A variable with different values and you want to check its value against multiple possible case
+
+let daysOfWeek = 10;
+//alert(daysOfWeek);
+switch (daysOfWeek) {
+    case 1:
+        console.log("Today is Monday ")
+        break;
+    case 2:
+        console.log("Today is Tuesday ")
+        break;
+    case 3:
+        console.log("Today is Wednessday ")
+        break;
+    case 4:
+        console.log("Today is Thursday ")
+        break;
+    case 5:
+        console.log("Today is Friday")
+        break;
+    case 6:
+        console.log("Today is Satuday ")
+        break;
+    case 7:
+        console.log("Today is Sunday ")
+        break;
+
+    default:
+        console.log("Not valid day");
+        break;
+}
+
+
+let age = 35;
+switch (true) {
+    
+    case (age>=20 && age<=30):
+        console.log("You are eligible");
+        break;
+    case (age>=30 || age<=20):
+        console.log("You are not eligible");
+        break;
+
+    default:
+        console.log("Enter valid age");
+        break;
+}
+
+
+ 
+// let x = '5' // string
+// let y = 10 // number
+// console.log(x + y); // '510' this is string (Type coercion) Implicit type conversion
 
 
 
+
+// Explicit type conversion
+//parseInt()
+
+// let x = '10'; // x is a string
+// let y = parseInt(x); // Explicitly convert x to an integer using parseInt()
+// console.log(y); // Output: 10
+// console.log(typeof y);
+
+let num = 15;
+let str = String(num);
+console.log(str);
+console.log(typeof str);
+
+
+let strr = "26";
+let numb = Number(strr);
+console.log(numb);
+console.log(typeof numb);
 
